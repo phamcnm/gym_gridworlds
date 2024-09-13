8ae5a14947e84c2572c027b3909a15518a21a27c
Minh Pham <phamcnm@Minhs-MacBook-Pro.local>
2024-09-13 14:37:47 -0600

    for plots: added color bar and fix tick params

--- assignment2.py
+++ assignment2.py
@@ -68,7 +68,7 @@ def bellman_q(init_value, policy, gamma, threshold):
         errors.append(error)
     return q_curr, errors
 
-
+label_size = 7
 policy = [1, 2, 4, 1, 2, 3, 2, 2, 3] # optimal policy, manually defined
 gammas = [0.01, 0.5, 0.99]
 for init_value in [-10, 0, 10]:
@@ -77,12 +77,17 @@ for init_value in [-10, 0, 10]:
     for i, gamma in enumerate(gammas):
         v, errors = bellman_v(init_value, policy, gamma, 0.000001)
         tile_values = np.array(v).reshape((3, 3))
-        axs[0][i].imshow(tile_values)
+        im = axs[0][i].imshow(tile_values)
+        cbar = fig.colorbar(im, ax=axs[0][i], fraction=0.046, pad=0.03)
+        cbar.ax.tick_params(labelsize=label_size)
+        cbar.ax.yaxis.set_major_locator(MaxNLocator(integer=True, nbins='auto')) 
+        axs[0][i].set_xticks([])
+        axs[0][i].set_yticks([])
         axs[1][i].plot(errors)
         axs[0][i].set_title(f'$\gamma$ = {gamma}')
         axs[1][i].xaxis.set_major_locator(MaxNLocator(integer=True, nbins='auto'))
-        axs[1][i].tick_params(axis='x', labelsize=5)
-        axs[1][i].tick_params(axis='y', labelsize=5)
+        axs[1][i].tick_params(axis='x', labelsize=label_size)
+        axs[1][i].tick_params(axis='y', labelsize=label_size)
 
     fig, axs = plt.subplots(n_actions + 1, len(gammas))
     fig.suptitle(f"$Q_0$: {init_value}")
@@ -92,14 +97,18 @@ for init_value in [-10, 0, 10]:
         for a in range(n_actions):
             axs[a][0].set_ylabel(row_labels[a], rotation=0, labelpad=40, fontsize=12, va='center')
             tile_values = np.array([q[s*n_actions+a] for s in range(n_states)]).reshape((3, 3))
-            axs[a][i].imshow(tile_values)
+            im = axs[a][i].imshow(tile_values)
+            cbar = fig.colorbar(im, ax=axs[a][i], fraction=0.046, pad=0.03)
+            cbar.ax.tick_params(labelsize=label_size)
+            cbar.ax.yaxis.set_major_locator(MaxNLocator(integer=True, nbins='auto')) 
             axs[a][i].set_xticks([])
             axs[a][i].set_yticks([])
         axs[-1][i].plot(errors)
         axs[0][i].set_title(f'$\gamma$ = {gamma}')
-        axs[0][i].tick_params(axis='x', labelsize=6)
-        axs[0][i].tick_params(axis='y', labelsize=6)
+        axs[0][i].tick_params(axis='x', labelsize=label_size)
+        axs[0][i].tick_params(axis='y', labelsize=label_size)
         axs[n_actions][i].xaxis.set_major_locator(MaxNLocator(integer=True, nbins='auto'))
+        axs[n_actions][i].tick_params(axis='x', labelsize=label_size)
+        axs[n_actions][i].tick_params(axis='y', labelsize=label_size)
         
     plt.show()
-    
